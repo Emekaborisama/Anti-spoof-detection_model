bd7ae8b718a1369893b75ae05d847cbf14b31db1
emekaborisama <borisphiloosphy@gmail.com>
2021-06-15 20:10:07 +0100

    3

--- app.py
+++ app.py
@@ -4,21 +4,31 @@ import time
 import requests
 import json
 from PIL import Image
+import os
+here = os.path.dirname(__file__)
+
+from tensorflow import keras
+import numpy as np
+np.random.seed(42)
+import tensorflow as tf
+
+modelpath= os.path.join(here, 'mob_netmodel_saved.h5')
+def load_pred(image_inf):
+    model2 = keras.models.load_model(modelpath, compile=False)
+    image = tf.keras.preprocessing.image.load_img(image_inf, target_size= (96, 96, 3))
+    input_arr = keras.preprocessing.image.img_to_array(image)
+    input_arr = np.array([input_arr])  # Convert single image to a batch.
+    predictions = model.predict(input_arr)
+    preds = np.argmax(predictions[0])
+    if preds == 1:
+        return("real")
+    elif preds == 0:
+        return("pls retake the profile image")
 
 
 
 
 
-
-def endpoint1(text):
-    url = "https://ilistener2-emekaborisama.cloud.okteto.net/named_entity"
-    payload={'text': text}
-    files=[
-
-    ]
-    headers = {}
-    response = requests.request("POST", url, headers=headers, data=payload, files=files)
-    return (response.text)
 
 
 
@@ -45,6 +55,12 @@ input_image= st.file_uploader("upload image or using your camera")
 if input_image is not None:
     image = Image.open(input_image)
     st.image(image, caption='Uploaded Image.', use_column_width=True)
+
+if st.button("Classify"):
+    tt = load_pred(image_inf = image)
+    st.title("Prediction")
+    st.markdown(tt)
+
 
 
 
---
+++ oppo.py
@@ -0,0 +1,9 @@
+"""def endpoint1(text):
+    url = "https://ilistener2-emekaborisama.cloud.okteto.net/named_entity"
+    payload={'text': text}
+    files=[
+
+    ]
+    headers = {}
+    response = requests.request("POST", url, headers=headers, data=payload, files=files)
+    return (response.text)"""

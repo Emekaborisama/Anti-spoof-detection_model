36b6f2a58365741eab5ba41a1ff5fa52460741f1
emekaborisama <borisphiloosphy@gmail.com>
2021-07-08 14:13:19 +0100

    up

--- app/api.py
+++ app/api.py
@@ -8,7 +8,7 @@ from flask import Flask, redirect, url_for, request, render_template
 from werkzeug.utils import secure_filename
 from gevent.pywsgi import WSGIServer
 from flask_cors import CORS, cross_origin
-
+import cv2
 
 app = Flask(__name__)
 cors = CORS(app)
@@ -23,7 +23,22 @@ def endpoint1(image_path):
     #print("time taken:", time.time() - starttime)
     return(results.text)
 
-
+def crop_Casa(imagepath):
+    img = cv2.imread(imagepath)
+    # Convert into grayscal
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Load the cascade
+    face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    # Draw rectangle around the faces and crop the faces
+    for (x, y, w, h) in faces:
+	    cv2.rectangle(img, (x, y), (x+w, y+h), (0, 0, 255), 2)
+	    faces = img[y:y + h, x:x + w]
+	    cv2.imshow("img", faces)
+	    tt = cv2.imwrite('face.jpg', faces)
+    
+    return tt
 
 
 @app.route('/Image_Classification', methods=['GET'])
@@ -39,6 +54,7 @@ def upload():
         image = Image.open(f)
         im1 = image.save("geeks.jpg")
         file_path = "geeks.jpg"
+        file_path2= "face.jpg"
 
 
         """basepath = os.path.dirname(__file__)
@@ -47,6 +63,7 @@ def upload():
         f.save(file_path)"""
 
         # Make prediction
-        preds = endpoint1(file_path)
+        crop_Casa(file_path)
+        preds = endpoint1(file_path2)
         return preds
     return None
